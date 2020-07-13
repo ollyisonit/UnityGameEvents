969200f7ba4a22210c05c17ec62f320d53864570
Olly <dninosores@gmail.com>
2020-07-13 15:49:26 -0700

    Added game event that loops another game event.

---
+++ Actions/LoopGameEvent.cs
@@ -0,0 +1,58 @@
+﻿using dninosores.UnityAccessors;
+using System.Collections;
+
+namespace dninosores.UnityGameEvents
+{
+	/// <summary>
+	/// Runs another game event a specified number of times.
+	/// </summary>
+	public class LoopGameEvent : GameEvent
+	{
+		/// <summary>
+		/// GameEvent to loop.
+		/// </summary>
+		public GameEvent gameEvent;
+		/// <summary>
+		/// Should the amount of times to run be recalcuated after each loop?
+		/// </summary>
+		public bool dynamic;
+		/// <summary>
+		/// Number of times to loop gameEvent.
+		/// </summary>
+		public IntOrConstantAccessor timesToRun;
+		protected override bool InstantInternal => gameEvent.Instant;
+
+		private bool cancelled;
+
+		public override void Stop()
+		{
+			cancelled = true;
+			gameEvent.Stop();
+		}
+
+		protected override IEnumerator RunInternal()
+		{
+			int loopCount = timesToRun.Value;
+			cancelled = false;
+			for (int i = 0; i < loopCount; i++)
+			{
+				if (dynamic)
+				{
+					loopCount = timesToRun.Value;
+				}
+				if (cancelled)
+				{
+					break;
+				}
+				if (gameEvent.Instant)
+				{
+					gameEvent.RunInstant();
+				}
+				else
+				{
+					yield return gameEvent.Run();
+				}
+			}
+		}
+	}
+}
