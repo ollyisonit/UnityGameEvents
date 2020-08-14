10c02a919ed1e016624e721260b8b0b26b2b146b
Olly <dninosores@gmail.com>
2020-08-13 21:57:00 -0700

    Added SetCursorLockMode event

---
+++ Actions/SetCursorLockMode.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace dninosores.UnityGameEvents.Actions
+{
+	/// <summary>
+	/// Sets lock mode of cursor.
+	/// </summary>
+	public class SetCursorLockMode : InstantGameEvent
+	{
+		public CursorLockMode mode;
+		protected override void InstantEvent()
+		{
+			Cursor.lockState = mode;
+		}
+	}
+}
